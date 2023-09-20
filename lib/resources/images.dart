0d4f6375659af45fcc4613e6139ed3e24ab8693b
YashwanthAnnavarapu <yashwanthannavarapua9@gmail.com>
2023-09-20 22:11:30 +0530

    Add logo widget

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -3,6 +3,7 @@ class Images {
   static const calendar = 'assets/icons/calendar.png';
 
   ///Images
+  static const logo = 'assets/images/home.png';
   static const home = 'assets/images/home.png';
 
   ///Json
